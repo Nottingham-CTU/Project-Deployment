cc0ae129b3c4ae5f2f3732280c7a812d8b623e7f
Richard Dooley <richard.dooley@nottingham.ac.uk>
2026-03-10 14:52:03 +0000

    Amended tests for new framework.

--- tests/test_01-Project_object_download.py
+++ tests/test_01-Project_object_download.py
@@ -23,6 +23,7 @@ class Test_01_Project_object_download:
     assert len(self.driver.find_elements(By.XPATH, "//*[@id=\"table-proj_table\"][contains(.,'Project Deployment Test 2')]")) > 0
     self.driver.find_element(By.LINK_TEXT, "Project Deployment Test 1").click()
     self.driver.find_element(By.CSS_SELECTOR, "a[href*=\"ExternalModules/\"][href*=\"prefix=project_deployment\"]").click()
+    self.driver.execute_script("//SAVEDESC:Verify project object.")
     self.driver.execute_script("$.get(window.location.href.replace('performdeployment','projectexport'),function(data,status,xhr){if(data[0].name=='GlobalVariables'&&data[1].name=='MetaDataVersion'&&data[0].items[0].name=='StudyName'&&data[0].items[0].data=='Project Deployment Test 1'&&xhr.getResponseHeader('Content-Type')=='application/json'){$('body').attr('data-exp-ok','1')};$('body').attr('data-exp','1')})")
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "body[data-exp]")))
     assert len(self.driver.find_elements(By.CSS_SELECTOR, "body[data-exp-ok]")) > 0
